6d648c821ca40f3a3437a3383d6e8672a4195a47
William Sousa <will_ssa@outlook.com>
2020-11-12 02:14:47 -0300

    Adding codePush

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import * as codePush from 'react-native-code-push'
 import { Provider } from 'react-redux'
 
 import { PersistGate } from 'redux-persist/integration/react'
@@ -7,6 +8,10 @@ import Routes from './routes'
 import { store, persistor } from './store'
 
 export default () => {
+  useEffect(() => {
+    codePush.default.sync({ deploymentKey: 'YB-Vh7yI1gr071lIMs-yCFSEliZLEhfAM7-7h' })
+  }, [])
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
