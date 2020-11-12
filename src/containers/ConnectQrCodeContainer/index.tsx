fac409283678ad1837843ddc541bd7972422df77
William Sousa <will_ssa@outlook.com>
2020-11-12 00:12:08 -0300

    Alter label

--- src/containers/ConnectQrCodeContainer/index.tsx
+++ src/containers/ConnectQrCodeContainer/index.tsx
@@ -54,7 +54,7 @@ export default ({ navigation }: any) => {
           cameraStyle={styles.cameraStyle}
           bottomContent={
             <View style={styles.footer}>
-              <Text style={styles.message}>Leia o QrCode do servidor</Text>
+              <Text style={styles.message}>Leia o Qr-code do servidor</Text>
             </View>
           }
         />
